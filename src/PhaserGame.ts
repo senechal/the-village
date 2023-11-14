c6f23856b8c58e366e06630fa3136ed67b405d11
André Luiz Lé Sénéchal Paolino <andre.senechal@hotmail.com>
2023-11-14 20:52:13 +0000

    Add map to scene.

--- src/PhaserGame.ts
+++ src/PhaserGame.ts
@@ -3,14 +3,14 @@ import Phaser from 'phaser'
 import { Game } from './scenes'
 
 const config: Phaser.Types.Core.GameConfig = {
-  // type: Phaser.AUTO,
-  // parent: 'phaser-container',
-  // backgroundColor: '#282c34',
-  // scale: {
-  //   mode: Phaser.Scale.ScaleModes.RESIZE,
-  //   width: window.innerWidth,
-  //   height: window.innerHeight,
-  // },
+  type: Phaser.AUTO,
+  parent: 'phaser-container',
+  backgroundColor: '#f1f1f1',
+  scale: {
+    mode: Phaser.Scale.ScaleModes.RESIZE,
+    width: window.innerWidth,
+    height: window.innerHeight
+  },
   // physics: {
   //   default: 'arcade',
   //   arcade: {
@@ -18,10 +18,6 @@ const config: Phaser.Types.Core.GameConfig = {
   //     debug: true,
   //   },
   // },
-  type: Phaser.AUTO,
-  parent: 'phaser-example',
-  width: 800,
-  height: 600,
   scene: [Game]
 }
 
--- src/scenes/Game/Game.ts
+++ src/scenes/Game/Game.ts
@@ -1,23 +1,23 @@
 import Phaser from 'phaser'
-import logoImg from './assets/logo.png'
+import tiles from './assets/tiles.png'
 
 export class Game extends Phaser.Scene {
+  constructor() {
+    super('SceneMain')
+  }
+
   preload(): void {
-    this.load.image('logo', logoImg)
-    this.load.image('background', 'assets/imgs/bg.jpg')
+    this.load.image('tiles', tiles)
+    this.load.tilemapTiledJSON('map', '/assets/tileMaps/map.json')
   }
 
   create(): void {
-    this.add.image(400, 300, 'background')
-    const logo = this.add.image(400, 150, 'logo')
-
-    this.tweens.add({
-      targets: logo,
-      y: 450,
-      duration: 2000,
-      ease: 'Power2',
-      yoyo: true,
-      loop: -1
+    const map = this.make.tilemap({
+      key: 'map',
+      tileWidth: 64,
+      tileHeight: 32
     })
+    const tileset = map.addTilesetImage('map', 'tiles')
+    map.createLayer('field', tileset, window.innerWidth / 2 - 32, 0)
   }
 }
